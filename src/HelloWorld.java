ed68cf0358806d31a09510642cb286165ea4ce89
Nunzio Mio <nunzio.mio92@gmail.com>
2022-09-02 14:08:14 +0200

    modifiche

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,8 @@
 public class HelloWorld {
     public static void main(String[] args) {
+        HelloHeaven hello = new HelloHeaven("message", "virus");
         System.out.println("hello world");
+        System.out.println(hello.getVirus());
+        System.out.println(hello.getMessage());
     }
 }
