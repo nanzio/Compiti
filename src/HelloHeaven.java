3ef3d8eff3da6ae4fccc877c38630d379a5b8bc4
Nunzio Mio <nunzio.mio92@gmail.com>
2022-09-02 14:59:07 +0200

    change to test conflict

--- src/HelloHeaven.java
+++ src/HelloHeaven.java
@@ -4,7 +4,7 @@ public class HelloHeaven {
 
     public HelloHeaven(String message, String virus) {
         this.message = message;
-        this.virus = virus;
+        this.virus = "44";
     }
 
     public HelloHeaven() {
